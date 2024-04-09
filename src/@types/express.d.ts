fe822808b217e9f11ea3f3b2d418db41410f47cc
Henefisa <jickyetran@outlook.com>
2024-04-09 13:56:24 -0400

    feat: refresh token API

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,7 +1,7 @@
-// import { User } from 'src/typeorm';
+import { User } from 'src/typeorm';
 
-// declare module 'express' {
-//   export interface Request {
-//     user: User;
-//   }
-// }
+declare module 'express' {
+  export interface Request {
+    user: User;
+  }
+}
